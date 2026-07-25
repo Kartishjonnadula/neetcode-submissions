563d5ba4507a22c1a0f07416a12d17fc7696a7d4
Jonnadula Kartish <48092149+Kartishjonnadula@users.noreply.github.com>
2026-07-25 12:00:32 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        from collections import defaultdict
+        if len(t)>len(s):
+            return ""
+        missing=len(t)
+        d=defaultdict(int)
+        for i in t:
+            d[i]+=1
+        start,ans=0,s
+        for end in range(0, len(s)):
+            d[s[end]]-=1
+            if d[s[end]]>=0:
+                missing-=1
+            while missing==0:
+                curr=s[start:end+1]
+                ans=curr if len(curr)<len(ans) else ans
+                d[s[start]]+=1
+                if d[s[start]]>0:
+                    missing+=1
+                start+=1
+        return ans
+                
+            
+        

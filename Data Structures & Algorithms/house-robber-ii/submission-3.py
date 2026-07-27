8931d4be845ee97f9aca7ebaae12b34962ce9299
Jonnadula Kartish <48092149+Kartishjonnadula@users.noreply.github.com>
2026-07-27 22:24:33 +0530

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n=len(nums)
+        if n==1:
+            return sum(nums)
+        from functools import cache
+        @cache
+        def solve(i,n):
+            if i>=n:
+                return 0
+            ans1,ans2=0,0
+            ans1=nums[i]+solve(i+2,n)
+            ans2=solve(i+1,n)
+            return max(ans1,ans2)
+        return max(solve(0,n-1),solve(1,n))
